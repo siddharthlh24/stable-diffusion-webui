832d3ced6716c86436cf8a24ffedb30a0fad52f1
Siddharth L Hasanabadi <siddharth.lh@gmail.com>
2025-01-21 20:30:29 -0500

    * update custom branch

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --listen
 
+set COMMANDLINE_ARGS= --xformers --listen --enable-insecure-extension-access
+REM set COMMANDLINE_ARGS=--precision full --no-half --lowvram --always-batch-cond-uncond --xformers
 call webui.bat
